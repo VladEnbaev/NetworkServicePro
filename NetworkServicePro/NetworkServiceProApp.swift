8249712797cb41926db34e738c7a355c6c651436
VladGradskiy <devvlad.gradskiy@gmail.com>
2023-12-24 19:15:08 +0300

    Initial Commit

---
+++ NetworkServicePro/NetworkServiceProApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkServiceProApp.swift
+//  NetworkServicePro
+//
+//  Created by Влад Енбаев on 24.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct NetworkServiceProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
