c133e8dedf8fad12bfe5f866bfa5640ddfe64ede
VladGradskiy <devvlad.gradskiy@gmail.com>
2023-12-26 14:00:03 +0300

    created service with possible data race

--- NetworkServicePro/ContentView.swift
+++ NetworkServicePro/ContentView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let networkService = NetworkService()
+    let paths = ["one", "two", "three", "four", "five", "six", "seven", "nine", "ten"]
+    let priorities: [TaskPriority] = [.background, .high, .low, .medium]
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -16,6 +21,20 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear {
+            Task {
+                for path in paths {
+                    let randomDuration = Int.random(in: 50...100)
+                    let randomPriority = priorities.randomElement()
+                    try? await Task.sleep(for: .nanoseconds(randomDuration))
+                    Task.detached(priority: randomPriority) {
+                        print("\(path): TASK_DETACHED priority: \(Task.currentPriority), interval: \(randomDuration), thread: \(Thread.current)")
+                        let data = try await networkService.request(path: path)
+                        print("Some data for \(path): \(data)")
+                    }
+                }
+            }
+        }
     }
 }
 
---
+++ NetworkServicePro/NetworkService.swift
@@ -0,0 +1,94 @@
+//
+//  NetworkService.swift
+//  NetworkServicePro
+//
+//  Created by Влад Енбаев on 24.12.2023.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case nonAuthorized
+}
+
+class NetworkService {
+    private var tokenRefreshingTask: Task<Void, Error>? = nil {
+        didSet {
+            print(tokenRefreshingTask)
+            
+        }
+    }
+    
+    init() { }
+    
+    func request(path: String) async throws -> String{
+        print("\(path): REQUEST_STARTED")
+        if let tokenRefreshingTask {
+            print("\(path): AWAITING_FOR_REFRESHING...")
+            try? await tokenRefreshingTask.value
+        }
+        
+        do {
+            let data = try await execute(path: path)
+            return data
+        } catch {
+            print("\(path): ERROR_OCCURED - \(error)")
+            guard let error = error as? NetworkError,
+                    error == .nonAuthorized else { throw error }
+            
+            if tokenRefreshingTask == nil {
+                print("\(path): REFRESHING_INIT")
+                tokenRefreshingTask = createTokenRefreshingTask(for: path)
+            }
+            
+            return try await request(path: path + "/second-entry")
+        }
+    }
+    
+    private func createTokenRefreshingTask(for path: String) -> Task<Void, Error> {
+        Task {
+            print("\(path): REFRESHING...")
+            await refreshTokenRequest()
+            tokenRefreshingTask = nil
+            print("\(path): REFRESHING_ENDED")
+            return
+        }
+    }
+    
+    private func execute(path: String) async throws -> String {
+        print("\(path): EXECUTE_STARTED...")
+        try await ServerForTest.shared.getData()
+        print("\(path): EXECUTE_SUCCESFUL")
+        return "ok"
+    }
+    
+    private func refreshTokenRequest() async {
+        try? await ServerForTest.shared.refreshToken()
+    }
+}
+
+
+actor ServerForTest {
+    
+    static var shared = ServerForTest()
+    var isNeedToRefreshToken = false
+    private var hasThrownNonAuthorizedError = false
+    
+    private init() {}
+    
+    func getData() async throws {
+        guard !isNeedToRefreshToken else { throw NetworkError.nonAuthorized }
+        if !hasThrownNonAuthorizedError {
+            isNeedToRefreshToken = true
+            hasThrownNonAuthorizedError = true
+            throw NetworkError.nonAuthorized
+        } else {
+            try await Task.sleep(for: .seconds(2))
+        }
+    }
+    
+    func refreshToken() async throws {
+        try await Task.sleep(for: .seconds(2))
+        isNeedToRefreshToken = false
+    }
+}
