97246205ef1e6c6e208f9dc7c492ce03d6219fe1
VladGradskiy <devvlad.gradskiy@gmail.com>
2024-02-13 18:30:04 +0300

    created package

---
+++ NetworkServicePro/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "NetworkServicePro",
+    platforms: [.iOS(.v13), .macOS(.v10_15), .watchOS(.v6), .tvOS(.v13)],
+    products: [
+        .library(
+            name: "NetworkServicePro",
+            targets: ["NetworkServicePro"]),
+    ],
+    targets: [
+        .target(
+            name: "NetworkServicePro"),
+        .testTarget(
+            name: "NetworkServiceProTests",
+            dependencies: ["NetworkServicePro"]),
+    ]
+)
